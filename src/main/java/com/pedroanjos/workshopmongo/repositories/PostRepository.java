136b3e6ebcec6ed9237a41a93bb4fac1471ce551
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2021-09-13 15:53:11 -0300

    Full search

--- src/main/java/com/pedroanjos/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/pedroanjos/workshopmongo/repositories/PostRepository.java
@@ -1,8 +1,10 @@
 package com.pedroanjos.workshopmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedroanjos.workshopmongo.domain.Post;
@@ -11,5 +13,8 @@ import com.pedroanjos.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2} }, { $or: [ { 'title': {$regex: ?0, $options: 'i' } }, { 'body': {$regex: ?0, $options: 'i' }, { 'comments.text': {$regex: ?0, $options: 'i' } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
